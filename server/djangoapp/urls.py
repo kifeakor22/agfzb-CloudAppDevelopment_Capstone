9516994fd440f05b2bce3cb6136f0b0a06dbbf02
kifeakor22 <113043752+kifeakor22@users.noreply.github.com>
2022-09-12 12:04:24 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path("home", views.home, name="home"),
 
-    # path for about view
+    path("about", views.about, name="about"),
 
-    # path for contact us view
+    path("contact", views.contact, name="contact"),
 
     # path for registration
 
